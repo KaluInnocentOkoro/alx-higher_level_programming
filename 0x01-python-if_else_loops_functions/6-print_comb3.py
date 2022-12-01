62dfd4afe7243bcb6482801eade878278144a2ad
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-12-01 05:52:28 +0100

    message

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range(9):
+    for j in range(i + 1, 10):
+        if i == 8 and j == 9:
+            print("{}{}".format(i % 10, j %     10))
+        else:
+            print("{}{}, ".format(i % 10, j % 10), end="")
