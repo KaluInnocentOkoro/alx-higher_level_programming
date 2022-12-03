7a6e5c54dc8b7333a0f8b0621f9807ca95aacbcb
Innocent Kalu <kaluinnocentokoro@gmail.com>
2022-12-03 07:31:41 +0100

    add tasks

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    size = len(sys.argv)
+    if size - 1 == 0:
+        print("{} arguments.".format(size - 1))
+    elif size - 1 == 1:
+        print("{} argument:".format(size - 1))
+        print("{}: {}".format(size - 1, sys.argv[1]))
+    else:
+        print("{} arguments:".format(size - 1))
+        for idx in range(1, size + 1):
+            print("{}: {}".format(idx, sys.argv[idx]))
