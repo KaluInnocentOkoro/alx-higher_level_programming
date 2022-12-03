e247afbc7a60c44f054624bad50642422f2900aa
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-12-03 12:20:57 +0100

    message

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,21 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
-    import sys
-    size = len(sys.argv)
-    if size != 4:
+    from sys import argv
+
+    size = len(argv)
+    if size < 4 or size > 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        sys.exit(1)
-    a = int(sys.argv[1])
-    b = int(sys.argv[3])
-    op = sys.argv[2]
-    operators = ['+', '-', '*', '/']
-    if op in operators:
-        if op == '+':
-            print("{} {} {} = {}".format(a, op, b, add(a, b)))
-        if op == '-':
-            print("{} {} {} = {}".format(a, op, b, sub(a, b)))
-        if op == '*':
-            print("{} {} {} = {}".format(a, op, b, mul(a, b)))
-        if op == '/':
-            print("{} {} {} = {}".format(a, op, b, div(a, b)))
-        sys.exit(0)
+        exit(1)
+
+    if size == 4:
+        a = int(argv[1])
+        b = int(argv[3])
+        op = argv[2]
+        operators = {'+': add, '-': sub, '*': mul, '/': div}
+        if op in operators:
+            ans = operators[op](a, b)
+            print("{} {} {} = {}".format(a, op, b, ans))
     else:
         print("Unknown operator. Available operators: +, -, * and /")
-        sys.exit(1)
+        exit(1)
