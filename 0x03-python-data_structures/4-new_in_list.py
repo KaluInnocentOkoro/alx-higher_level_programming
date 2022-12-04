f4b103c50b12260952b9341f5538e5218bca6c16
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-12-04 15:29:59 +0100

    message

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,14 +1,5 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    """Function replaces an element at a specific position
-    without modifying the original list
-    Args:
-        my_list: (list)
-        idx: The required position
-        element: element to be replaced
-
-    Returns: A new list
-    """
     size = len(my_list)
     new_list = my_list[:]
     if idx > size or idx < 0:
