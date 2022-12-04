87fabf85486fd8eae5cb7d07a9480bd5566fa396
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-12-04 15:32:19 +0100

    message

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    size = len(my_list)
     new_list = my_list[:]
-    if idx > size or idx < 0:
+    if idx < 0 or idx > len(my_list):
         return new_list
     new_list[idx] = element
     return new_list
