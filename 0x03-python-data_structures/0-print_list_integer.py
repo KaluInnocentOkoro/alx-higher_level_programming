d9424c21986e7419ef46f21824c66b2a06621e92
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-12-04 15:20:10 +0100

    updated task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -6,6 +6,5 @@ def print_list_integer(my_list=[]):
 
     Returns: Nothing
     """
-    if my_list:
-        for ele in my_list:
-            print("{}".format(ele))
+    for ele in my_list:
+        print("{:d}".format(ele))
