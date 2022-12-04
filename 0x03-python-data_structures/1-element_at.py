45d715ec0454d687c858b7df10a9a47938686397
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-12-04 15:23:21 +0100

    updated task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,7 +3,8 @@ def element_at(my_list, idx):
     """retrives an element from a list
     Args:
         my_list: (list)
-        idx: (int) position of tge element to be retrieved
+        idx: (int) position of the element to be retrieved
+
     Returns: my_list[idx] or None
     """
     size = len(my_list)
