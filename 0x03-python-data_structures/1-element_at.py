721eb84c16e90ad41f79e7b4ed143f3ec69a7e16
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-12-04 15:45:02 +0100

    message

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,12 +1,5 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    """retrives an element from a list
-    Args:
-        my_list: (list)
-        idx: (int) position of the element
-
-    Returns: (list) my_list[idx] or None
-    """
     if idx < 0 or idx > len(my_list):
         return None
     return my_list[idx]
