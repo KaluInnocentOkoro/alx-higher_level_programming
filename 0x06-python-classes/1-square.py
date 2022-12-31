70515c9ddfdf04c7eaffe8de8c6b159e1f919c8e
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-12-31 22:35:00 +0100

    message

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Describes a square class"""
+
+
+class Square:
+    """Defines a square object"""
+    def __init__(self, size):
+        """initializes an instance of the square class
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
