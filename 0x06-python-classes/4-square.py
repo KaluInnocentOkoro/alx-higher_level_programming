20c6ae0fb3ce05da339342f674881897fa9064a1
Kalu Innocent <obinnasmartkalu@gmail.com>
2022-12-31 23:22:46 +0100

    add task 4

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""Describes a square class"""
+
+
+class Square:
+    """Defines a square object"""
+    def __init__(self, size=0):
+        """initializes an instance of the square class
+        Args:
+            size (int): size of the square
+        """
+        self.__size = size
+
+    def area(self):
+        """defines the area of a square"""
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        """retrieves the size of the square"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """sets the size of the square
+        Args:
+            value (int): value of the square size
+        """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
