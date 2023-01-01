1eea2b87acb39d648f669026c1f7ddc6f7a79e8b
Innocent Kalu <kaluinnocentokoro@gmail.com>
2023-01-01 10:53:19 +0100

    update task 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -11,8 +11,8 @@ class Square:
             size (int): size of the square
             position (tuple): coordinates of tge square
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """defines the area of a square"""
@@ -31,10 +31,9 @@ class Square:
         """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("size must be >= 0")
-        else:
-            self.__size = value
+        self.__size = value
 
     @property
     def position(self):
@@ -58,11 +57,13 @@ class Square:
         """Public instance method that prints the square with to stdout
         """
         if self.__size == 0:
-            print("")
+            print()
             return
-        for i in range(self.__position[1]):
+        for i in range(0, self.__position[1]):
             print("")
-        for idx in range(self.__size):
-            for j in range(self.__position[0]):
+        for idx in range(0, self.__size):
+            for j in range(0, self.__position[0]):
                 print(" ", end="")
-            print("#" * self.__size)
+            for k in range(0, self.__size):
+                print("#", end="")
+            print("")
