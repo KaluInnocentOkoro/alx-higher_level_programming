0395070637fd6959bd92d3361906bd58b535d1f8
Innocent Kalu <kaluinnocentokoro@gmail.com>
2023-01-10 23:30:13 +0000

    add task 1

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,11 @@
 #!/usr/bin/python3
+"""Object Methods"""
+
+
 def lookup(obj):
+    """Function returns object attributes and methods
+    Args:
+        obj (class):
+    Returns: list of object Attributes
+    """
     return dir(obj)
