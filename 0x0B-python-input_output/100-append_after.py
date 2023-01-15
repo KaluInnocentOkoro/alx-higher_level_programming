d6cd119fef735bbd5e6872d9211800010757ea92
Innocent Kalu <kaluinnocentokoro@gmail.com>
2023-01-15 14:01:53 +0000

    add task 13

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/pyton3
+"""appends a text"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Function inserts a line of text to a file,
+    after each line containing a specific string
+    Args:
+        filename (txt): file to be appended to
+        search_string (str): string to be searched
+        new_string (str): string to be appended
+    """
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+    with open(filename, 'w') as file:
+        for line in lines:
+            file.write(line)
+            if search_string in line:
+                file.write(new_string)
