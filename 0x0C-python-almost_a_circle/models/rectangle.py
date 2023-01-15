e0b0f4aaa0a4a231bd3c1a5ce94d6ac5b51861f3
Innocent Kalu <kaluinnocentokoro@gmail.com>
2023-01-15 20:26:02 +0000

    add task 6

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -84,3 +84,13 @@ class Rectangle(Base):
         """Prints a graphical representation of rectangle object"""
         for i in range(self.__height):
             print("#" * self.__width)
+
+    def __str__(self):
+        return "[{}] ({}) {}/{} - {}/{}".format(
+            __class__.__name__,
+            self.id,
+            self.__x,
+            self.__y,
+            self.__width,
+            self.__height
+        )
