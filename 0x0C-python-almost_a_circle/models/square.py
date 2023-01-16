7abb4446833716f24b99735a14841511740bae96
Innocent Kalu <kaluinnocentokoro@gmail.com>
2023-01-16 00:22:22 +0000

    add task 10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""A square class"""
+from rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """Defines a Square class"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """Instantiates a Square object
+        Args:
+            size (int): size of square
+            x (int)
+            y (int)
+            id (any)
+        """
+        super().__init__(size, size, x, y, id)
